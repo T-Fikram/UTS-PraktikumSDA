f650b5a3c06775bbca23a67136924fd60a3d139b
Teuku Fikram Al Syahbanna <teukufikramalsyahbanna@gmail.com>
2025-03-18 11:35:32 +0700

    Tambahin header dan Fungsi Prototype

--- parsing_expression.c
+++ parsing_expression.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+#define MAX 100
+
+// Prototype
+void infixToPostfix(char infix[], char postfix[]);
+void infixToPrefix(char infix[], char prefix[]);
+void postfixToInfix(char postfix[], char infix[]);
+void prefixToInfix(char prefix[], char infix[]);
+void prefixToPostfix(char prefix[], char postfix[]);
+void postfixToPrefix(char postfix[], char prefix[]);
