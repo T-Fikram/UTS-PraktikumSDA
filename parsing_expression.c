b16d3042ddb4090db24245e17ed726ebbf792cfc
M. Rayan Zishan <147892080+Zishan06@users.noreply.github.com>
2025-03-19 06:28:46 +0700

    tamah fungsi InfixKeuPostfix

--- parsing_expression.c
+++ parsing_expression.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
-#include<conio.h>
 
 #define MAX 100
+#define OPERAND(ch) ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) //ini untuk mengecek operand
 
-// Struktur data untuk stack, dipakai buat simpan operator pas konversi
 typedef struct{
     int top;
     char item[MAX];
@@ -21,13 +20,25 @@ bool cek_penuh(Stack *s){
     return(s->top == MAX - 1);
 }
 void push(Stack *s, char value){
-    s->item[++(s->top)] == value;
+    s->item[++(s->top)] = value;
 }
-void pop(Stack *s, char value){
-    s->item[(s->top)--];
+char pop(Stack *s){
+    return s->item[(s->top)--];
 }
 char peek(Stack *s){
     return s->item[s->top];
+}
+int prioritas(char op) {
+    switch (op) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        default:
+            return 0;
+    }
 }
 
 // Prototype
@@ -46,24 +57,63 @@ int main() {
     //keknya nanti kita buat perulangan aja di sini
     printf("1. Infix ke Postfix\n2. Infix ke Prefix\n-------------------------\n");
     printf("Ahoi! Nak buat ape?\n");
-    // Ambil pilihan dari user 
-    pilihan = getch();
+    scanf("%d", &pilihan);
     switch(pilihan){
-        case 1 || 2:
+        case 1:
+        case 2:
             printf("tamong operasi infix di sini\n");
-            scanf("%s", ekspresi);
+            fgets(ekspresi, MAX, stdin);
             if(pilihan == 1){
                 infixToPostfix(ekspresi, hasil);
                 printf("Hasil nibak peurubahan nyan nakeuh lagèe nyoe : %s\n", hasil);
             }
             else if(pilihan == 2){
-                infixToPrefix(ekspresi, hasil);
+                //infixToPrefix(ekspresi, hasil);
                 printf("Hasil nibak peurubahan nyan nakeuh lagèe nyoe : %s\n", hasil);
             }
-        
-        case 3 || 4:
-            //kita rehat sejenak, btw emang bisa bentuk switchnya macam tu? 
+            break;
+        case 3:
+        case 4:
+            //kita rehat sejenak
 
         default:
     }
+    return 0;
+}
+
+void infixToPostfix(char infix[], char postfix[]){
+    Stack *s;
+    inisialisasi(s);
+    int j, i;
+
+    while(infix[i] != '\0'){
+
+        char ch = infix[i];
+
+        if(OPERAND(ch)) postfix[j++] = ch; //kalau jumpa operand berarti tambahkan kepostfix
+
+        else if(ch == '(')push(s, ch);//kalau jumpa ni (, masukkan ke stack
+
+        else if(ch == ')'){
+            while(!cek_kosong(s) && peek(s) != '('){
+               postfix[j++] = pop(s);//kalau jumpa ni ), popkan ni (
+            }
+            pop(s);
+        }
+        else{
+            while(!cek_kosong && prioritas(peek(s)) >= prioritas(ch)){
+               postfix[j++] = pop(s);//mengecek tingkatan operator
+            }
+            push(s, ch);
+        } i++;
+        while (!cek_kosong(s)) {
+           postfix[j++] = pop(s);
+        }
+
+      
+
+
+    }
+    postfix[j] = '\0'; 
+
 }
